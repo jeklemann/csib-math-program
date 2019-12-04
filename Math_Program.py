c5b8e3d201b1e1ad050a65ceecbcb0ded08bcdb2
jeklemann <jeklemann@protonmail.ch>
2019-12-04 10:09:03 -0600

    Create functions instead because neat

--- Math_Program.py
+++ Math_Program.py
@@ -4,12 +4,24 @@ import random
 easy=["Addition","Subtraction"]
 medium=["Addition","Subtraction","Multiplication","Divison"]
 
+def addition():
+    integer1 = random.randint(0, 10)
+    integer2 = random.randint(0, 10)
+    print(integer1, "+", integer2)
+    answer = input(" - ")
+    if answer == integer1 + integer2:
+        print("correct")
+
+def subtraction():
+    return
+
+def multiplication():
+    return
+
+def division():
+    return
 #easy
 option=random.choice(easy)
 if option="Addition":
-    integer1=random.randint(0,10)
-    integer2=random.randint(0,10)
-    print(integer1,"+",integer2)
-    answer=input(" - ")
-    if answer==integer1+integer2:
-        print("correct")
+    addition()
+
